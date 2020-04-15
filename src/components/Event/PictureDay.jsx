b8036ba0a6fab062330273f09669d2acfc72507d
TheCats100 <m-g-152@outlook.fr>
2020-04-15 14:41:11 +0200

    style(pictureday): new RBnB style

--- src/components/Event/PictureDay.jsx
+++ src/components/Event/PictureDay.jsx
@@ -1,39 +1,41 @@
 import React from 'react'
 import Axios from 'axios'
 
-class PictureDay extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            url: "https://via.placeholder.com/550",
-            concept: "Reste à l'écran"
-        }
-        this.getPicture = this.getPicture.bind(this)
-    }
+class PictureDay extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      url: "https://via.placeholder.com/550",
+      concept: "Reste à l'écran"
+    };
+    this.getPicture = this.getPicture.bind(this);
+  }
 
-    componentDidMount() {
-        this.getPicture();
-    } 
+  componentDidMount() {
+    this.getPicture();
+  }
 
     getPicture = () => {
-        Axios.get("https://api.nasa.gov/planetary/apod?api_key=tJEyrCHFpmMVohJmDqxBnDac7xXMWQeEUeYNIcKc")
-            .then((response) => response.data)
-            .then((data) => {
-                this.setState({
-                    url: data.url,
-                    concept: data.explanation
-                })
-            })
+      Axios.get("https://api.nasa.gov/planetary/apod?api_key=tJEyrCHFpmMVohJmDqxBnDac7xXMWQeEUeYNIcKc")
+        .then((response) => response.data)
+        .then((data) => {
+          this.setState({
+            url: data.url,
+            concept: data.explanation,
+          });
+        });
     }
 
-    render(){
-        return(
-            <>
-                <img src={this.state.url} alt=""/>
-                <p>{this.state.concept}</p>
-            </>
-        )
+    render() {
+      const { url } = this.state;
+      const { concept } = this.state;
+      return (
+        <>
+          <img src={url} alt="" />
+          <p>{concept}</p>
+        </>
+      );
     }
 }
 
-export default PictureDay
+export default PictureDay;
