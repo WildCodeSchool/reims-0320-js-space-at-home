167f5164ec02f4f6a21c0bfce114ef88a0148987
Brian <brian.lagaude@gmail.com>
2020-04-15 11:54:05 +0200

    feat: add Satelites Folder and Moon File

---
+++ src/components/Satelites/Moons.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Moons extends React.Component{
+    constructor(){
+        super()
+        this.state(test)
+    }
+    render(){
+        return(
+            <h1>Moons</h1>
+            <ul>
+                <li>Earth : Moon</li>
+                <li>Mars : Phobos, Deimos</li>
+                <li>Jupiter : IO, Europe, Ganymède, Callisto</li>
+                <li>Saturne: Mimas, Encelade, Téthys, Dioné, Rhéa, Titan, Hyperion, Japet, Phoebé</li>
+                <li>Uranus : Puck , Miranda, Ariel, Umbriel, Titania, Obéron</li>
+                <li>Neptune  : Triton,, Néréide</li>
+                <li>PLuton : Charon</li>
+            </ul>
+        )
+    }
+}
+
+export default Moons
