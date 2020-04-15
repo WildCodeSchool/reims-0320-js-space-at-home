2e25663532081c20192fbe232462dc7594c1a3f8
TheCats100 <m-g-152@outlook.fr>
2020-04-15 17:39:42 +0200

    feat(app): try modification header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import PictureDay from './components/Event/PictureDay';
 
-
 function App() {
   return (
-    <div>
-      <PictureDay />
-    </div>
+    <>
+      <div className="pageBlock">
+        <PictureDay />
+      </div>
+    </>
   );
 }
+
+/*
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Space at Home</title>
+<link rel="shortcut icon" href="https://avatars2.githubusercontent.com/u/8874047?s=280&v=4" type="image/x-icon" />
+</head>
+*/
+
 
 export default App;
