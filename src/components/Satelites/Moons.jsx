05b4fa7103fc7f5b101e2647c7c72349abf7f7e0
Brian <brian.lagaude@gmail.com>
2020-04-15 15:01:38 +0200

    refactor: adapt to EsLint

--- src/components/Satelites/Moons.jsx
+++ src/components/Satelites/Moons.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-class Moons extends React.Component{
-    constructor(props){
-        super(props)
-        this.state(test)
-    }
-    render(){
-        return(
-            <h1>Moons</h1>
-            <ul>
-                <li>Earth : Moon</li>
-                <li>Mars : Phobos, Deimos</li>
-                <li>Jupiter : IO, Europe, Ganymède, Callisto</li>
-                <li>Saturne: Mimas, Encelade, Téthys, Dioné, Rhéa, Titan, Hyperion, Japet, Phoebé</li>
-                <li>Uranus : Puck , Miranda, Ariel, Umbriel, Titania, Obéron</li>
-                <li>Neptune  : Triton,, Néréide</li>
-                <li>PLuton : Charon</li>
-            </ul>
-        )
-    }
+class MoonsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Earth : Moon</li>
+          <li>Mars : Phobos, Deimos</li>
+          <li>Jupiter : IO, Europe, Ganymède, Callisto</li>
+          <li>Saturne: Mimas, Encelade, Téthys, Dioné, Rhéa, Titan, Hyperion, Japet, Phoebé</li>
+          <li>Uranus : Puck , Miranda, Ariel, Umbriel, Titania, Obéron</li>
+          <li>Neptune  : Triton,, Néréide</li>
+          <li>PLuton : Charon</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default Moons
+export default MoonsList;
