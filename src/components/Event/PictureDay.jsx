277e7c518ac3fc69416f8da441f09a68a1ffc2dc
TheCats100 <m-g-152@outlook.fr>
2020-04-15 12:14:10 +0200

    feat(pictureday): add apod with explanation and picture

--- src/components/Event/PictureDay.jsx
+++ src/components/Event/PictureDay.jsx
@@ -1,21 +1,36 @@
 import React from 'react'
+import Axios from 'axios'
 
 class PictureDay extends React.Component{
     constructor(){
         super()
         this.state = {
-            url: "https://via.placeholder.com/150",
-            concept: null
+            url: "https://via.placeholder.com/550",
+            concept: "Reste à l'écran"
         }
+        this.getPicture = this.getPicture.bind(this)
     }
 
+    componentDidMount() {
+        this.getPicture();
+      } 
 
+    getPicture = () => {
+        Axios.get("https://api.nasa.gov/planetary/apod?api_key=tJEyrCHFpmMVohJmDqxBnDac7xXMWQeEUeYNIcKc")
+            .then((response) => response.data)
+            .then((data) => {
+                this.setState({
+                    url: data.url,
+                    concept: data.explanation
+                })
+            })
+    }
 
     render(){
         return(
             <>
             <img src={this.state.url} alt=""/>
-            <p>Affichage svp</p>
+            <p>{this.state.concept}</p>
             </>
         )
     }
