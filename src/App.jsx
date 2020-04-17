e1f2a6bfebcfb821b2ecc7cd14687726dcbbba9e
Brian <brian.lagaude@gmail.com>
2020-04-17 09:32:38 +0200

    feat: add categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PictureDay from './components/Event/PictureDay';
+import Categories from './components/Categories/Categories';
 
 function App() {
   return (
     <>
       <div className="pageBlock">
         <PictureDay />
+        <Categories />
       </div>
     </>
   );
---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './categories.css';
+
+class Categories extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  Opencard = () => onclick;
+
+  render() {
+    return (
+      <div className="OpenCard">
+        <br />
+        <div className="block">
+          <div className="Circle" />
+          <div className="CategoriesTitle">
+            <h2>Earth</h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tellus ipsum. Nunc sollicitudin rhoncus eleifend.
+            Pellentesque aliquet libero elit, egestas aliquet arcu molestie a.
+              </p>
+            <p>Vivamus id ex ut ante faucibus sodales quis at tellus.Proin lectus dolor, convallis non rutrum in, mollis nec magna.
+            Vestibulum pulvinar, urna et consectetur malesuada, est eros rutrum justo, eu aliquet enim tellus id quam.
+            Integer accumsan ex metus, finibus euismod eros imperdiet quis.Phasellus rhoncus turpis vehicula magna volutpat aliquam.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Categories;
