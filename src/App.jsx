401eb35457578c605758a44fc714c38a37d03547
TheCats100 <m-g-152@outlook.fr>
2020-04-22 16:01:37 +0200

    fix(app): merge correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
-import PictureDay from './components/Event/PictureDay';
-import Categories from './components/Categories/Categories';
-import SolarSystem from './components/SolarSystem/SolarSystem';
-import Satellites from './components/Satellites/Satellites';
-
 
 function App() {
   return (
     <>
       <div className="pageBlock">
         <Navbar />
-        <PictureDay />
-        <Categories />
-        <Satellites />
       </div>
     </>
   );
