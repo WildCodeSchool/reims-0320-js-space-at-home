146fe5a271353bdf39efdf8e32b206cadcb389d2
TheCats100 <m-g-152@outlook.fr>
2020-04-23 17:23:37 +0200

    feat(satellitescss): image satcom sputnik

--- src/components/Satellites/Explorer1.jsx
+++ src/components/Satellites/Explorer1.jsx
@@ -23,7 +23,7 @@ class Explorer1 extends React.Component {
             ? <iframe src="https://solarsystem.nasa.gov/gltf_embed/2386" title="Explorer 1" className="satellitesIframe" />
             : null}
         </div>
-        <div>
+        <div className="satellitesText">
           <h1>Explorer 1</h1>
           <p>Explorer 1 was the first satellite launched by the United States, and was part of the U.S. participation in the International Geophysical Year. The mission followed the first two satellites the previous year; the Soviet Union's Sputnik 1 and 2, beginning the Cold War Space Race between the two nations.</p>
           <p>Explorer 1 was launched on January 31, 1958, at 22:48 Eastern Time (February 1, 03:48 UTC) atop the first Juno booster from LC-26 at the Cape Canaveral Missile Annex, Florida. It was the first spacecraft to detect the Van Allen radiation belt,[2] returning data until its batteries were exhausted after nearly four months. It remained in orbit until 1970, and has been followed by more than 90 scientific spacecraft in the Explorer series.</p>
--- src/components/Satellites/SpaceStation.jsx
+++ src/components/Satellites/SpaceStation.jsx
@@ -23,7 +23,7 @@ class SpaceStation extends React.Component {
             ? <iframe src="https://solarsystem.nasa.gov/gltf_embed/2378" title="International Space" className="satellitesIframe" />
             : null}
         </div>
-        <div>
+        <div className="satellitesText">
           <h1>SpaceStation</h1>
           <p>The International Space Station (ISS) is a space station (habitable artificial satellite) in low Earth orbit. The ISS programme is a joint project between five participating space agencies: NASA (United States), Roscosmos (Russia), JAXA (Japan), ESA (Europe), and CSA (Canada). The ownership and use of the space station is established by intergovernmental treaties and agreements.</p>
           <p>The ISS serves as a microgravity and space environment research laboratory in which crew members conduct experiments in biology, human biology, physics, astronomy, meteorology, and other fields. The station is suited for the testing of spacecraft systems and equipment required for missions to the Moon and Mars. The ISS maintains an orbit with an average altitude of 400 kilometres (250 mi) by means of reboost manoeuvres using the engines of the Zvezda module or visiting spacecraft. It circles the Earth in roughly 92 minutes and completes 15.5 orbits per day.</p>
