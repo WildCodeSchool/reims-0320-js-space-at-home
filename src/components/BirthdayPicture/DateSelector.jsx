74175b2e7bbf8e90747672b2731d6e3c96ced21e
TheCats100 <m-g-152@outlook.fr>
2020-04-27 12:30:30 +0200

    feat(dsjsx): add date selector

--- src/components/BirthdayPicture/DateSelector.jsx
+++ src/components/BirthdayPicture/DateSelector.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import PictureDay from './components/BirthdayPicture/PictureDay';
+// import PictureDay from './components/BirthdayPicture/PictureDay';
 
-function DateSelector{
-    <div />
+function DateSelector() {
+  return (
+    <>
+      <div>
+        <label htmlFor="date">
+          BirthdayPicture
+          <input type="date" id="date" name="date" value="2018-07-22" min="1995-06-16" max="2020-04-01" />
+        </label>
+      </div>
+      <div>
+        <button type="button">Valider</button>
+      </div>
+    </>
+  );
 }
 
-export default DateSelector
+export default DateSelector;
