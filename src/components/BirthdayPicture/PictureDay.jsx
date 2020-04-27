92bc2a097ece244f1a060f1f14284f2083edce4f
TheCats100 <m-g-152@outlook.fr>
2020-04-27 12:29:51 +0200

    refactor(pdjsx): import

--- src/components/BirthdayPicture/PictureDay.jsx
+++ src/components/BirthdayPicture/PictureDay.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Axios from 'axios';
 import './pictureDay.css';
+import DateSelector from './DateSelector';
 
 class PictureDay extends React.Component {
   constructor() {
@@ -35,15 +36,18 @@ class PictureDay extends React.Component {
     render() {
       const { url, concept } = this.state;
       return (
-        <div className="eventPage">
-          <h1 className="eventTitle">Picture of The Day :</h1>
-          <div className="eventBlock">
-            {url.includes('youtube')
-              ? <iframe src={url} title="youtubeVideo" allowFullScreen="true" />
-              : <img className="eventImage" src={url} alt="pictureDay" />}
-            <p className="eventText">{concept}</p>
+        <>
+          <DateSelector />
+          <div className="eventPage">
+            <h1 className="eventTitle">Picture of The Day :</h1>
+            <div className="eventBlock">
+              {url.includes('youtube')
+                ? <iframe src={url} title="youtubeVideo" allowFullScreen="true" />
+                : <img className="eventImage" src={url} alt="pictureDay" />}
+              <p className="eventText">{concept}</p>
+            </div>
           </div>
-        </div>
+        </>
       );
     }
 }
