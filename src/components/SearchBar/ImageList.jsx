fcfdce7663d8c72e784cb5e8b1b6c7fea317c7ed
Brian <brian.lagaude@gmail.com>
2020-05-05 10:47:52 +0200

    feat: add grid to image call by api

--- src/components/SearchBar/ImageList.jsx
+++ src/components/SearchBar/ImageList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './SearchBar.css';
 
 const ImageList = ({ images }) => images.map((image) => {
   if (image.links == null) {
     return null;
   }
-  return <img src={image.links[0].href} alt="" />;
+  return <img src={image.links[0].href} className="ListImages" alt="" />;
 });
 export default ImageList;
