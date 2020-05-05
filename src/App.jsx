ea1a21d64653e301521efcc345acccb19b8b19cf
VictorV <victor.veyrier@icloud.com>
2020-05-05 10:40:06 +0200

    feat(Head): create Navbar Menu and burger

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import PictureDay from './components/BirthdayPicture/PictureDay';
 import './App.css';
 import Instructions from './components/Instructions/Instructions';
 import ButtonTop from './components/ButtonTop/ButtonTop';
-
+import NavBarMB from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
       <div className="pageBlock">
+        <NavBarMB />
         <Instructions />
         <PictureDay />
         <ButtonTop />
