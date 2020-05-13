cbbb0e4486799c8cb6c4c5742ab7adb33e262fe3
TheCats100 <m-g-152@outlook.fr>
2020-05-13 17:13:46 +0200

    feat(searchb): add feature with wrong search

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,9 +14,9 @@ function Footer() {
         <div className="createdFooter">
           <h3>Created by :</h3>
           <a href="https://www.linkedin.com/in/martin-gilbert-23452b1a3/"><p>Martin Gilbert</p></a>
-          <a href="https://www.linkedin.com/in/victor-veyrier-ab6948194/"><p>Victor Veyrier</p></a>
           <a href="https://www.linkedin.com/in/brian-lagaude-4110b31a4/"><p>Brian Lagaude</p></a>
           <a href="https://www.linkedin.com/in/victor-auxietre-fontes-6a8439184/"><p>Victor Auxietre-Fontes</p></a>
+          <a href="https://www.linkedin.com/in/victor-veyrier-ab6948194/"><p>Victor Veyrier</p></a>
         </div>
         <div className="explanationFooter">
           <p>Created with React and using NASA APIs</p>
--- src/components/SearchBar/NasaAPI.jsx
+++ src/components/SearchBar/NasaAPI.jsx
@@ -8,15 +8,20 @@ import './SearchBar.css';
 const SearchImages = () => {
   const [images, setImages] = useState([]);
   const [keywords, setKeywords] = useState('');
+  const [badSearch, setBadSearch] = useState(false);
 
 
   const search = () => {
-    Axios.get(
-      `https://images-api.nasa.gov/search?q=${keywords}&media_type=image`,
-    )
+    Axios.get(`https://images-api.nasa.gov/search?q=${keywords}&media_type=image`)
       .then((response) => response.data)
       .then((data) => {
-        setImages(data.collection.items);
+        const image = data.collection.items;
+        if (image.length === 0) {
+          setBadSearch(true);
+        } else {
+          setBadSearch(false);
+          setImages(data.collection.items);
+        }
       });
   };
 
@@ -27,8 +32,12 @@ const SearchImages = () => {
         onSearch={search}
         setKeywords={setKeywords}
       />
-      <div className="ImageApi">
-        <ImageList images={images} className="ImageList" />
+      <div>
+        <ImageList
+          images={images}
+          badSearch={badSearch}
+          className="ImageList"
+        />
       </div>
     </div>
   );
