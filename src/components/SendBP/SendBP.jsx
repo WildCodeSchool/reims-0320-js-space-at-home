9a85dc68c78e5dea24b841935a6689a3807624c4
TheCats100 <m-g-152@outlook.fr>
2020-05-13 10:59:33 +0200

    feat(sbp): copy url with selector

--- src/components/SendBP/SendBP.jsx
+++ src/components/SendBP/SendBP.jsx
@@ -6,29 +6,52 @@ import './SendBP.css';
 class SendBP extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      urlSelect: '',
+    };
+    this.copyUrl = this.copyUrl.bind(this);
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    const { urlSelect } = this.state;
+    if (prevState.urlSelect !== urlSelect) {
+      this.copyUrl();
+    }
+  }
+
+  copyUrl = () => {
+    const copyText = document.getElementById('copyInput');
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+  };
+
   render() {
+    const { urlSelect } = this.state;
     const { favorites } = this.props;
-    const result = favorites.map((favorite) => favorite.url).join('\n');
-    const copyAllUrl = () => {
-      const copyText = document.getElementById('copyInput');
-      copyText.select();
-      copyText.setSelectionRange(0, 99999);
-      document.execCommand('copy');
-    };
 
     return (
       <div>
-        <label htmlFor="selectPicture">
-          <select name="selectPicture" id="selectPicture">
-            <option>Choose an option</option>
-            {favorites.map((picture) => <option>{picture.chosenName}</option>)}
-          </select>
-        </label>
-        <input className="noneInput" type="textarea" value={result} id="copyInput" />
-        <button type="button" onClick={() => copyAllUrl()}>Copy Urls</button>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            const selectIn = document.getElementById('selectPicture');
+            const choice = selectIn.selectedIndex;
+            const urlSelectIn = selectIn.options[choice].value;
+            this.setState({ urlSelect: urlSelectIn });
+          }}
+        >
+          <label htmlFor="selectPicture">
+            <select name="selectPicture" id="selectPicture" className="sbpSelect">
+              <option value="" disabled selected hidden>Choose a picture</option>
+              {favorites.map(
+                (picture) => <option value={picture.url}>{picture.chosenName}</option>,
+              )}
+            </select>
+          </label>
+          <button type="submit">Copy Urls</button>
+          <input className="noneInput" type="text" value={urlSelect} id="copyInput" readOnly="true" />
+        </form>
       </div>
     );
   }
