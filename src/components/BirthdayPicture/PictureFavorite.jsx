e38745f7f379eebc11da0ab76b8aa67b77cd9e95
Victorafx <victor.auxietrefontes@laposte.net>
2020-05-13 10:17:02 +0200

    perf(favoritepic): add css to picture favorite section

--- src/components/BirthdayPicture/PictureFavorite.jsx
+++ src/components/BirthdayPicture/PictureFavorite.jsx
@@ -6,12 +6,15 @@ const PictureFavorite = ({ favorites }) => {
     <div className="galleryBlock">
       {favorites.map((favorite) => {
         return (
-          <div>
-            <h1>{favorite.chosenName}</h1>
+          <div className="cardPicture">
             {favorite.url.includes('youtube')
               ? <iframe className="eventIframe" src={favorite.url} title="youtubeVideo" allowFullScreen />
               : <img className="galleryImage" src={favorite.url} alt="" />}
-            <p className="galleryConcept">{favorite.concept}</p>
+            <h3 className="nameImage">{favorite.chosenName}</h3>
+            <details>
+              <summary className="seeConcept">voir plus</summary>
+              <p className="galleryConcept">{favorite.concept}</p>
+            </details>
           </div>
         );
       })}
